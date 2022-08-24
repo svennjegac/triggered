640ae19b8bdcc2dc91cf2cf25e5a0407d2930572
Sven Njegac <sven.njegac@gmail.com>
2022-08-24 16:11:15 +0200

    init

---
+++ trigg/commands/root.go
@@ -0,0 +1,32 @@
+package commands
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var RootCmd = &cobra.Command{
+	Use:     "trigg",
+	Version: "v1.0.0",
+	Run: func(cmd *cobra.Command, args []string) {
+		str := strings.Join(args, " ")
+		fmt.Println(str)
+
+		strTrigg := ""
+		lowerCase := true
+		for _, r := range str {
+			var s string
+			if lowerCase {
+				s = strings.ToLower(string(r))
+			} else {
+				s = strings.ToUpper(string(r))
+			}
+			strTrigg += s
+			lowerCase = !lowerCase
+		}
+
+		fmt.Println(strTrigg)
+	},
+}
---
+++ trigg/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/svennjegac/triggered/trigg/commands"
+)
+
+func main() {
+	commands.RootCmd.Execute()
+}
